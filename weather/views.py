72dfca43be7ded63f3e047add0df6208c3e4d4ca
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-05 15:44:57 +0300

    task done

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = "03e783d17d771bd83e3dc0916d5c153c"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = "Gomel"
+    res = requests.get(url.format(city))
+    print(res.text)
+
     return render(request, 'weather/index.html')
+
+    
+
 
 def test_page(request):
     return render(request, 'weather/test_page.html')
