f4b4b6aa38a1e4edc1ad68ec6fba504040c54c39
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-05 19:25:31 +0300

    task done

--- weather/views.py
+++ weather/views.py
@@ -3,16 +3,37 @@ import requests
 
 def index(request):
     appid = "03e783d17d771bd83e3dc0916d5c153c"
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid=" + appid
 
     city = "Gomel"
-    res = requests.get(url.format(city))
-    print(res.text)
+    res = requests.get(url.format(city)).json()
+    
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
 
-    return render(request, 'weather/index.html')
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
 
     
 
 
 def test_page(request):
-    return render(request, 'weather/test_page.html')
+    appid = "03e783d17d771bd83e3dc0916d5c153c"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid=" + appid
+
+    city = "Gomel"
+    res = requests.get(url.format(city)).json()
+    
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'description': res["weather"][0]["description"]
+    }
+
+    context = {'info': city_info}
+    
+    return render(request, 'weather/test_page.html', context)
